c989472dbf95ddb0067427911760467bd8d59ddb
jason <jason.p.mcelhenney@gmail.com>
2023-02-19 14:19:24 -0600

    v1.0.1
    added tkinter baremetal GUI threading code
    moved code around to make threaded GUI possible

---
+++ buy_listener.py
@@ -0,0 +1,166 @@
+import pika
+import dotenv
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
+import time
+import logging
+import json
+import configparser
+
+
+def buy_item_by_id(item_id, item_price, item_hash_name):
+    bot_inventory = driver.find_element(By.CLASS_NAME, "market-items")
+    item_to_buy = bot_inventory.find_elements(By.XPATH, f"//div[@data-id={item_id}]")
+
+    if len(item_to_buy) == 0:
+        logging.info(f"({item_hash_name}) - Cannot find this item in bot's inventory.")
+        return {
+            "success": False,
+            "message": "There is no such item in bot's inventory"
+        }
+    else:
+        wagered_balance = float(driver.find_element(By.ID, "withdraw-bal").text)
+
+        if wagered_balance < item_price: return {
+            "success": False,
+            "message": f"You do not have enough wagered balance for this operation. [wagered balance: ${wagered_balance}, price: ${item_price}]."
+        }
+
+        logging.info(f"({item_hash_name}) - Adding to cart.")
+        item_to_buy[0].click()
+        driver.find_element(By.ID, "buyItems").click()
+        # TODO: we shouldn't assume the purchase was successful
+        return {
+            "success": True,
+            "message": f"bought item {item_hash_name} for ${item_price}."
+        }
+
+
+def callback(ch, method, properties, body):
+    message_dict = json.loads(body)
+    item_hash_name = message_dict['item_hash_name']
+    item_id = message_dict['item_id']
+
+    buy_prices = message_dict['buy_prices']
+    buy_usd = buy_prices['usd']
+    buy_keys = buy_prices['keys']
+    buy_refs = buy_prices['refs']
+
+    sell_prices = message_dict['sell_prices']
+
+    buyer_id = sell_prices['steamid']
+
+    sell_usd = sell_prices['usd']
+    sell_keys = sell_prices['keys']
+    sell_refs = sell_prices['refs']
+
+    logging.info(f"({item_hash_name}) - Attempting to buy for ${buy_usd}.")
+
+    status = buy_item_by_id(item_id, buy_usd, item_hash_name)
+    if status['success'] is False:
+        logging.info(f"({item_hash_name}) - Can not complete order. {status['message']}")
+    else:
+        logging.info(f"({item_hash_name}) - item bought successfully.")
+
+    # Discord notification there
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+class BuyListener:
+    channel = None
+    config = None
+    pika_host = None
+    pika_port = None
+    pika_username = None
+    pika_password = None
+    pika_queue = None
+
+    def __init__(self, config, pika_host, pika_port, pika_username, pika_password):
+        self.config = config
+        self.pika_host = pika_host
+        self.pika_port = pika_port
+        self.pika_username = pika_username
+        self.pika_password = pika_password
+
+        self.main()
+
+    def main(self):
+        credentials = pika.PlainCredentials(self.pika_username, self.pika_password)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.pika_host, port=self.pika_port, credentials=credentials))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue=self.config['RABBITMQ']['queue'], durable=True)
+
+        # ---------------- Configure logging ---------------- #
+        logging.basicConfig(level="INFO",
+                            format='%(levelname)s %(asctime)s - %(message)s',
+                            datefmt='%H:%M:%S')
+        logging.getLogger('urllib3').setLevel(logging.WARNING)
+        logging.getLogger('selenium').setLevel(logging.WARNING)
+        logging.getLogger('undetected_chromedriver').setLevel(logging.WARNING)
+
+        # ---------------- Configure CLI flags ---------------- #
+        options = Options()
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--start-maximized")
+
+        # head = config['BROWSER']['method'].lower()
+
+        # make sure URL ends with a slash
+        scrape_url = self.config['SCRAPE']['url']
+        if not scrape_url.endswith("/"):
+            scrape_url = scrape_url + "/"
+
+        # ---------------- Login process ---------------- #
+        global driver
+        driver = uc.Chrome(options=options)
+        # if head == "headless": options.add_argument("--headless")
+        global wait
+        wait = WebDriverWait(driver, 60)
+        driver.get(scrape_url)
+
+        login_method = self.config['LOGIN']['method'].lower()
+
+        if login_method == "steam":
+            driver.find_element(By.CLASS_NAME, "login-block").click()
+            time.sleep(1)
+            wait.until(EC.title_contains("TF2EASY.COM"))
+        elif login_method == "cookie":
+            login_cookie = os.getenv("login_cookie")
+            driver.add_cookie({
+                "name": "laravel_session",
+                "value": login_cookie
+            })
+            driver.refresh()
+        else:
+            logging.info("You have to select login method in config.ini!")
+            return
+        try:
+            wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'menu-username-text')))
+        except:
+            logging.info("Couldn't login. (Wrong cookie?)")
+            return
+
+        logging.info("Logged in!")
+        driver.get(scrape_url + "market")
+
+        # wait for page to load
+        # TODO: probably want to fix this but it works for now
+        time.sleep(5)
+
+    def start(self):
+        # ---------------- Start listening for messages ---------------- #
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue=self.config['RABBITMQ']['queue'], on_message_callback=callback)
+
+        logging.info("Waiting for messages...")
+        self.channel.start_consuming()
+
+
+
+
--- main.py
+++ main.py
@@ -1,137 +1,65 @@
-import pika
 import dotenv
 import os
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-import undetected_chromedriver as uc
-import time
-import logging
-import json
 import configparser
+import tkinter
+import threading
 
-def buy_item_with_id(item_id, item_price, item_hash_name):
+from buy_listener import BuyListener
 
-    bot_inventory = driver.find_element(By.CLASS_NAME,"market-items")
-    item_to_buy = bot_inventory.find_elements(By.XPATH,f"//div[@data-id={item_id}]")
 
-    if len(item_to_buy) == 0:
-        logging.info(f"({item_hash_name}) - Can not find this item in bot's inventory.")
-        return {
-            "success" : False,
-            "message" : "There is no such item in bot's inventory"
-        }
-    else:
-        wagered_balance = float(driver.find_element(By.ID,"withdraw-bal").text)
+class GUI(threading.Thread):
+    def __init__(self, buy_listener):
+        super().__init__()
+        self.thread = None
+        self.buy_listener = buy_listener
+        self.root = tkinter.Tk()
 
-        if wagered_balance < item_price: return{
-            "success" : False,
-            "message" : f"You do not have enough wagered balance for this operation. [wagered balance: ${wagered_balance}, price: ${item_price}]."
-        }
+        self.root.title("TF2EZ_PYBUY")
+        self.root.geometry("300x400")
+        self.root.resizable(False, False)
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        logging.info(f"({item_hash_name}) - Adding to cart.")
-        item_to_buy[0].click()
-        driver.find_element(By.ID,"buyItems").click()
-        return{
-            "success" : True,
-            "message" : f""
-        }
+        self.label = tkinter.Label(self.root, text="TF2EZ_PYBUY")
+        self.label.pack()
 
-def callback(ch, method, properties, body):
-    message_dict = json.loads(body)
-    item_hash_name = message_dict['item_hash_name']
-    item_id = message_dict['item_id']
-    
-    buy_prices = message_dict['buy_prices']
-    buy_usd = buy_prices['usd']
-    buy_keys = buy_prices['keys']
-    buy_refs = buy_prices['refs']
+        self.button = tkinter.Button(self.root, text="Start", command=self.start)
+        self.button.pack()
 
-    sell_prices = message_dict['sell_prices']
+        self.root.mainloop()
 
-    buyer_id = sell_prices['steamid']
-     
-    sell_usd = sell_prices['usd']
-    sell_keys = sell_prices['keys']
-    sell_refs = sell_prices['refs']
+    def start(self):
+        self.thread = threading.Thread(target=self.thread_func)
+        self.thread.start()
 
-    logging.info(f"({item_hash_name}) - Attempting to buy for ${buy_usd}.")
-    
-    status = buy_item_with_id(item_id,buy_usd,item_hash_name)
-    if status['success'] is False: logging.info(f"({item_hash_name}) - Can not complete order. {status['message']}")
-    else: logging.info(f"({item_hash_name}) - item bought succesfully.")
+    def thread_func(self):
+        self.button["state"] = "disabled"
+        self.label["text"] = "running..."
+        # Do something
+        self.buy_listener.start()
 
-    # Discord notification there
+        self.label["text"] = "done"
+        self.button["state"] = "normal"
 
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    def on_closing(self):
+        self.root.destroy()
+
 
 def main():
-    dotenv.load_dotenv()
-    pika_host = os.getenv("pika_host")
-    pika_port = int(os.getenv("pika_port"))
-    pika_username = os.getenv("pika_username")
-    pika_password = os.getenv("pika_password")
-    credentials = pika.PlainCredentials(pika_username, pika_password)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=pika_host, port=pika_port, credentials=credentials))
-    channel = connection.channel()
-    channel.queue_declare(queue='stf-scrape_tf2ez',durable=True)
-
-    logging.basicConfig(level="INFO",
-                    format='%(levelname)s %(asctime)s - %(message)s',
-                    datefmt='%H:%M:%S')
-    logging.getLogger('urllib3').setLevel(logging.WARNING)
-    logging.getLogger('selenium').setLevel(logging.WARNING)
-    logging.getLogger('undetected_chromedriver').setLevel(logging.WARNING)
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue='stf-scrape_tf2ez', on_message_callback=callback)
-
-    options = Options()
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--start-maximized")
-
+    # ---------------- Load variables ---------------- #
     config = configparser.ConfigParser()
     config.read('config.ini')
+    dotenv.load_dotenv()
 
-    login_method = config['LOGIN']['method'].lower()
-    # head = config['BROWSER']['method'].lower()
+    # ---------------- Configure RabbitMQ ---------------- #
+    pika_host = os.getenv("PIKA_HOST")
+    pika_port = int(os.getenv("PIKA_PORT"))
+    pika_username = os.getenv("PIKA_USERNAME")
+    pika_password = os.getenv("PIKA_PASSWORD")
 
-    # if head == "headless": options.add_argument("--headless")
+    # ---------------- pass data to buy_listener ---------------- #
+    worker = BuyListener(config, pika_host, pika_port, pika_username, pika_password)
+    GUI(worker)
 
-    global driver
-    driver = uc.Chrome(options=options)
-    global wait
-    wait = WebDriverWait(driver,60)
-    driver.get("https://www.tf2easy.com/")
-
-    if login_method == "steam":
-        driver.find_element(By.CLASS_NAME,"login-block").click()
-        time.sleep(1)
-        wait.until(EC.title_contains("TF2EASY.COM"))
-    elif login_method == "cookie":
-        login_cookie = os.getenv("login_cookie")
-        driver.add_cookie({
-            "name" : "laravel_session",
-            "value" : login_cookie
-        })
-        driver.refresh()
-    else:
-        logging.info("You have to select login method in config.ini!")
-        return
-    try:
-        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'menu-username-text'))) 
-    except:
-        logging.info("Can not log in.")
-        return
-
-    logging.info("Logged in!")
-    driver.get("https://www.tf2easy.com/market")
-    
-    time.sleep(5)
-    
-    logging.info("Checking tf2ez inventory.")
-    channel.start_consuming()
 
 if __name__ == "__main__":
     main()
